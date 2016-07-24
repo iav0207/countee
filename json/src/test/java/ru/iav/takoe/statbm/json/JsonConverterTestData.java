239a9b8486a0c830228c07abeccfe5d2219f26c3
takoe <IsaevAV27@gmail.com>
2016-07-24 14:38:02 +0300

    Implemented JsonConverter

---
+++ json/src/test/java/ru/iav/takoe/statbm/json/JsonConverterTestData.java
@@ -0,0 +1,29 @@
+package ru.iav.takoe.statbm.json;
+
+import org.testng.annotations.DataProvider;
+import ru.iav.takoe.statbm.json.vo.TestObject;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static ru.iav.takoe.statbm.utils.DateUtils.now;
+import static ru.iav.takoe.statbm.utils.TestUtils.getRandomBoolean;
+import static ru.iav.takoe.statbm.utils.TestUtils.getRandomInteger;
+import static ru.iav.takoe.statbm.utils.TestUtils.getRandomString;
+
+/**
+ * Created by takoe on 24.07.16.
+ */
+class JsonConverterTestData {
+
+    static TestObject getTestObject() {
+        TestObject object = new TestObject();
+        object.setUuid(UUID.randomUUID());
+        object.setaBoolean(getRandomBoolean());
+        object.setInteger(getRandomInteger());
+        object.setString(getRandomString());
+        object.setDate(now());
+        return object;
+    }
+
+}
