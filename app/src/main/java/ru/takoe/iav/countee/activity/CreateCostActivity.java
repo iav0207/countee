07b3c2df46ad9727cec2669dca4beef109a64f90
takoe <IsaevAV27@gmail.com>
2016-08-21 16:43:39 +0300

    Implemented new navigation view

--- app/src/main/java/ru/takoe/iav/countee/activity/CreateCostActivity.java
+++ app/src/main/java/ru/takoe/iav/countee/activity/CreateCostActivity.java
@@ -1,6 +1,10 @@
 package ru.takoe.iav.countee.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -14,9 +18,8 @@ import ru.iav.takoe.countee.service.SaveCostService;
 import ru.takoe.iav.countee.R;
 import ru.takoe.iav.countee.properties.ApplicationProperties;
 
-public class CreateCostActivity extends AppCompatActivity {
-
-    private final DrawerLayoutRenderer drawerLayoutRenderer = new DrawerLayoutRenderer(this);
+public class CreateCostActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,15 @@ public class CreateCostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_cost);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -33,8 +45,6 @@ public class CreateCostActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
-
-        drawerLayoutRenderer.renderDrawer();
 
         ApplicationProperties.setOutputDirectory(getFilesDir());
         updateOutputText();
@@ -49,7 +59,7 @@ public class CreateCostActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_cost, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -66,6 +76,25 @@ public class CreateCostActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_stats) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else {
+            // do nothing
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     /**
