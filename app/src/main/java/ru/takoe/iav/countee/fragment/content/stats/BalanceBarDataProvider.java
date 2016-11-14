867569eec17b728502e33f912983a3081bbf0e28
takoe <IsaevAV27@gmail.com>
2016-11-14 21:10:50 +0300

    Extracted color generation to a separate class

--- app/src/main/java/ru/takoe/iav/countee/fragment/content/stats/BalanceBarDataProvider.java
+++ app/src/main/java/ru/takoe/iav/countee/fragment/content/stats/BalanceBarDataProvider.java
@@ -1,12 +1,10 @@
 package ru.takoe.iav.countee.fragment.content.stats;
 
 import android.content.res.AssetManager;
-import android.graphics.Color;
 import android.graphics.Typeface;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import org.joda.time.DateTime;
 import ru.iav.takoe.countee.service.FundsDataService;
 
@@ -14,21 +12,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static ru.iav.takoe.countee.utils.TestUtils.getRandomInteger;
-
 /**
  * Created by takoe on 12.11.16.
  */
 public class BalanceBarDataProvider {
 
-    private static final int[] color_base = ColorTemplate.PASTEL_COLORS;
-
     private static final String caption = "Funds, daily";
+
+    private BarDataColorGenerator colorGenerator;
 
     private Typeface typeface;
 
     public BalanceBarDataProvider(AssetManager assets) {
         this.typeface = Typeface.createFromAsset(assets, "fonts/OpenSans-Regular.ttf");
+        this.colorGenerator = new BarDataColorGenerator();
     }
 
     public BarData getFundsBarData() {
@@ -39,43 +36,8 @@ public class BalanceBarDataProvider {
 
     private BarDataSet createDataSet() {
         BarDataSet dataSet = new BarDataSet(createEntries(), caption);
-        setDataColors(dataSet);
+        colorGenerator.setDataColor(dataSet);
         return dataSet;
-    }
-
-    private void setDataColors(BarDataSet dataSet) {
-        float minValue = dataSet.getYMin();
-        float maxValue = dataSet.getYMax();
-        int[] colors = new int[dataSet.getEntryCount()];
-        for (int i = 0; i < colors.length; i++) {
-            float y = dataSet.getEntryForIndex(i).getY();
-            int red = Math.round( (maxValue - y) / (maxValue - minValue) ) * 195 + 30;
-            int green = 30;
-            int blue = Math.round( (y - minValue) / (maxValue - minValue) ) * 195 + 30;
-            colors[i] = mergeWithColorBase(Color.rgb(red, green, blue));
-        }
-        dataSet.setColors(colors);
-    }
-
-    private int mergeWithColorBase(int color) {
-        int baseColor = color_base[getRandomInteger(color_base.length)];
-        return Color.rgb(
-                avg(Color.red(color), Color.red(baseColor)),
-                avg(Color.green(color), Color.green(baseColor)),
-                avg(Color.blue(color), Color.blue(baseColor))
-        );
-    }
-
-    private int avg(int one, int two) {
-        return Math.round((one + two) / 2);
-    }
-
-    private int[] getColorBase(int count) {
-        int[] colors = new int[count];
-        for (int i = 0; i < count; i++) {
-            colors[i] = color_base[i % color_base.length];
-        }
-        return colors;
     }
 
     private List<BarEntry> createEntries() {
---
+++ app/src/main/java/ru/takoe/iav/countee/fragment/content/stats/BarDataColorGenerator.java
@@ -0,0 +1,68 @@
+package ru.takoe.iav.countee.fragment.content.stats;
+
+import android.graphics.Color;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import static ru.iav.takoe.countee.utils.TestUtils.getRandomInteger;
+
+/**
+ * Created by takoe on 14.11.16.
+ */
+class BarDataColorGenerator {
+
+    private static final int PRIMARY_COLOR = Color.parseColor("#9982BA");
+
+    private static final int[] COLOR_BASE = ColorTemplate.MATERIAL_COLORS;
+
+    void setDataColor(final BarDataSet dataSet) {
+        dataSet.setColor(getRandomBaseColor());
+    }
+
+    void setDataColors(final BarDataSet dataSet) {
+        int[] colors = new int[dataSet.getEntryCount()];
+        for (int i = 0; i < colors.length; i++) {
+            int colorByValue = getColorByValue(min(dataSet), max(dataSet), getValue(dataSet, i));
+            colors[i] = merge(PRIMARY_COLOR, merge(getRandomBaseColor(), colorByValue));
+        }
+        dataSet.setColors(colors);
+    }
+
+    private static float min(IBarDataSet dataSet) {
+        return dataSet.getYMin();
+    }
+
+    private static float max(IBarDataSet dataSet) {
+        return dataSet.getYMax();
+    }
+
+    private static float getValue(IBarDataSet dataSet, int index) {
+        return dataSet.getEntryForIndex(index).getY();
+    }
+
+    private static int getColorByValue(float min, float max, float value) {
+        return Color.rgb(
+                Math.round( (max - value) / (max - min) ) * 195 + 30,
+                30,
+                Math.round( (value - min) / (max - min) ) * 100 + 100
+        );
+    }
+
+    private static int merge(int color1, int color2) {
+        return Color.rgb(
+                avg(Color.red(color1), Color.red(color2)),
+                avg(Color.green(color1), Color.green(color2)),
+                avg(Color.blue(color1), Color.blue(color2))
+        );
+    }
+
+    private static int avg(int one, int two) {
+        return Math.round((one + two) / 2);
+    }
+
+    private static int getRandomBaseColor() {
+        return COLOR_BASE[getRandomInteger(COLOR_BASE.length)];
+    }
+
+}
