f924fe05d39f76a967ed9e7f7faaceefba5e974e
takoe <IsaevAV27@gmail.com>
2016-12-01 21:01:37 +0300

    Implemented service providing data to build cost charts

--- service/src/main/java/ru/iav/takoe/countee/service/ChartsDataService.java
+++ service/src/main/java/ru/iav/takoe/countee/service/ChartsDataService.java
@@ -2,10 +2,14 @@ package ru.iav.takoe.countee.service;
 
 import org.joda.time.DateTime;
 import ru.iav.takoe.countee.da.CostReader;
+import ru.iav.takoe.countee.model.filter.impl.CostCommentFilter;
+import ru.iav.takoe.countee.model.strategy.CostsDailyStrategy;
+import ru.iav.takoe.countee.model.strategy.CostsMonthlyStrategy;
 import ru.iav.takoe.countee.model.strategy.FundsDailyStrategy;
 import ru.iav.takoe.countee.model.strategy.FundsMonthlyStrategy;
 import ru.iav.takoe.countee.vo.Cost;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 import java.util.Map;
 
@@ -28,12 +32,29 @@ public class ChartsDataService {
         return instance;
     }
 
+    @Nonnull
     public Map<DateTime, Float> getFundsDailyData() {
         return new FundsDailyStrategy(getAllCosts()).execute();
     }
 
+    @Nonnull
     public Map<DateTime, Float> getFundsMonthlyData() {
         return new FundsMonthlyStrategy(getAllCosts()).execute();
+    }
+
+    @Nonnull
+    public Map<DateTime, Float> getCostsDailyData(@Nonnull String... commentsToFilter) {
+        return new CostsDailyStrategy(getFilteredCosts(commentsToFilter)).execute();
+    }
+
+    @Nonnull
+    public Map<DateTime, Float> getCostsMonthlyData(@Nonnull String... commentsToFilter) {
+        return new CostsMonthlyStrategy(getFilteredCosts(commentsToFilter)).execute();
+    }
+
+    @Nonnull
+    private List<Cost> getFilteredCosts(@Nonnull String... comments) {
+        return CostCommentFilter.from(comments).filter(getAllCosts());
     }
 
     private List<Cost> getAllCosts() {
