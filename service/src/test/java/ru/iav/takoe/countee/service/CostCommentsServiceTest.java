bcd227ad58c71556190519ac8d7c11cda38320b2
takoe <IsaevAV27@gmail.com>
2016-12-01 20:49:20 +0300

    Implemented CostCommentsService

---
+++ service/src/test/java/ru/iav/takoe/countee/service/CostCommentsServiceTest.java
@@ -0,0 +1,104 @@
+package ru.iav.takoe.countee.service;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.iav.takoe.countee.da.CostReader;
+import ru.iav.takoe.countee.vo.Cost;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * Created by takoe on 01.12.16.
+ */
+public class CostCommentsServiceTest {
+
+    @Mock
+    private CostReader reader;
+
+    @InjectMocks
+    private CostCommentsService service;
+
+    private Set<String> result;
+
+    @BeforeClass
+    public void init() {
+        initMocks(this);
+    }
+
+    @BeforeMethod
+    public void reset() {
+        Mockito.reset(reader);
+    }
+
+    @Test
+    public void shouldReturnEmptySetIfReaderReturnedNull() throws Exception {
+        letReaderReturn(null);
+        execute();
+        assertResultIsEmpty();
+    }
+
+    @Test
+    public void shouldReturnEmptySetIfReaderReturnedEmptyList() throws Exception {
+        letReaderReturn(new ArrayList<Cost>());
+        execute();
+        assertResultIsEmpty();
+    }
+
+    @Test(dataProvider = "lowerCaseComments", dataProviderClass = CostCommentsServiceTestData.class)
+    public void shouldReturnAllCommentsDistinct(List<Cost> costs, Set<String> expected) throws Exception {
+        runTestWithData(costs, expected);
+    }
+
+    @Test(dataProvider = "differentCaseComments", dataProviderClass = CostCommentsServiceTestData.class)
+    public void shouldCastCommentsToLowerCase(List<Cost> costs, Set<String> expected) throws Exception {
+        runTestWithData(costs, expected);
+    }
+
+    @Test(dataProvider = "disorderedComments", dataProviderClass = CostCommentsServiceTestData.class)
+    public void shouldArrangeCommentsAlphabetically(List<Cost> costs, List<String> expected) throws Exception {
+        letReaderReturn(costs);
+        execute();
+        Iterator<String> iteratorExpected = expected.iterator();
+        Iterator<String> iteratorActual = result.iterator();
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(iteratorActual.next(), iteratorExpected.next());
+        }
+        assertEquals(result.size(), expected.size());
+    }
+
+    private void runTestWithData(List<Cost> costs, Set<String> expected) {
+        letReaderReturn(costs);
+        execute();
+        assertResultIsEqualTo(expected);
+    }
+
+    private void execute() {
+        result = service.getAllCommentsSet();
+    }
+
+    private void letReaderReturn(List<Cost> costs) {
+        doReturn(costs).when(reader).readAllCosts();
+    }
+
+    private void assertResultIsEmpty() {
+        assertNotNull(result);
+        assertEquals(result.size(), 0);
+    }
+
+    private void assertResultIsEqualTo(Set<String> expected) {
+        assertEquals(result, expected);
+    }
+
+}
---
+++ service/src/test/java/ru/iav/takoe/countee/service/CostCommentsServiceTestData.java
@@ -0,0 +1,49 @@
+package ru.iav.takoe.countee.service;
+
+import org.testng.annotations.DataProvider;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static ru.iav.takoe.countee.service.utils.CounteeTestUtils.listOfCostsWithComments;
+
+/**
+ * Created by takoe on 01.12.16.
+ */
+public class CostCommentsServiceTestData {
+
+    @DataProvider(name = "lowerCaseComments")
+    public static Object[][] lowerCaseComments() {
+        return new Object[][] {{
+            listOfCostsWithComments("a", "a", "a", "b", "12", "-&щ", ""),
+                set("a", "b", "12", "-&щ", "")
+        }};
+    }
+
+    @DataProvider(name = "differentCaseComments")
+    public static Object[][] differentCaseComments() {
+        return new Object[][] {{
+                listOfCostsWithComments("a", "A", "a", "b", "12", "-&Щ", "fHhgO i?", ""),
+                set("a", "b", "12", "-&щ", "", "fhhgo i?")
+        }};
+    }
+
+    @DataProvider(name = "disorderedComments")
+    public static Object[][] disorderedComments() {
+        return new Object[][] {{
+                listOfCostsWithComments("c", "b", "a", "-&Щ", "", "a", "12", "c", "Ъ"),
+                list("", "-&щ", "12", "a", "b", "c", "ъ")
+        }};
+    }
+
+    private static Set<String> set(String... comments) {
+        return new TreeSet<>(list(comments));
+    }
+
+    private static List<String> list(String... comments) {
+        return Arrays.asList(comments);
+    }
+
+}
