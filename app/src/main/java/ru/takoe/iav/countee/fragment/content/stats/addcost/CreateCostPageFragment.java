a795c30e34aa2e22d6e75cad23c4327f93136a3f
takoe <IsaevAV27@gmail.com>
2016-12-05 01:09:01 +0300

    Implemented swipe view @ main screen

---
+++ app/src/main/java/ru/takoe/iav/countee/fragment/content/stats/addcost/CreateCostPageFragment.java
@@ -0,0 +1,51 @@
+package ru.takoe.iav.countee.fragment.content.stats.addcost;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import ru.iav.takoe.countee.service.CostOutputService;
+import ru.takoe.iav.countee.R;
+import ru.takoe.iav.countee.view.ViewProvider;
+import ru.takoe.iav.countee.view.ViewScroller;
+
+/**
+ * Created by takoe on 03.09.16.
+ */
+public class CreateCostPageFragment extends Fragment {
+
+    private TextView outputArea;
+
+    private ScrollView scrollView;
+
+    private int position;
+
+    public static CreateCostPageFragment newInstance(int position) {
+        CreateCostPageFragment fragment = new CreateCostPageFragment();
+        fragment.position = position;
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_create_cost_output, container, false);
+        outputArea = (TextView) rootView.findViewById(R.id.output_text);
+        scrollView = (ScrollView) rootView.findViewById(R.id.scrollableOutputText);
+        updateOutputText();
+        return rootView;
+    }
+
+    private void updateOutputText() {
+        outputArea.setText(getReadCostService().getOutputForPrevMonth(position));
+        ViewScroller.scrollToBottom(scrollView);
+    }
+
+    private CostOutputService getReadCostService() {
+        return CostOutputService.getInstance();
+    }
+
+}
