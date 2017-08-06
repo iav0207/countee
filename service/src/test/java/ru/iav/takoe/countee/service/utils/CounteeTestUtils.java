d6b6b5e2737cdfcd97abca3008573bb4edb728c3
Alexander Isaev <vakoy@yandex-team.ru>
2017-08-06 23:00:56 +0300

    Fix: displaying only cost names which have non-zero total amount over all time

--- service/src/test/java/ru/iav/takoe/countee/service/utils/CounteeTestUtils.java
+++ service/src/test/java/ru/iav/takoe/countee/service/utils/CounteeTestUtils.java
@@ -1,14 +1,15 @@
 package ru.iav.takoe.countee.service.utils;
 
-import org.joda.time.DateTime;
-import ru.iav.takoe.countee.vo.Cost;
-
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.Mockito.doReturn;
+import org.joda.time.DateTime;
+import ru.iav.takoe.countee.vo.Cost;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static ru.iav.takoe.countee.utils.TestUtils.getRandomBigDecimal;
 import static ru.iav.takoe.countee.utils.TestUtils.getRandomDateOfLastYear;
 import static ru.iav.takoe.countee.utils.TestUtils.getRandomString;
@@ -25,16 +26,23 @@ public class CounteeTestUtils {
 
     public static Cost getRandomCost() {
         Cost mock = mock(Cost.class);
-        doReturn(getRandomBigDecimal()).when(mock).getAmount();
-        doReturn(getRandomString()).when(mock).getComment();
-        doReturn(getRandomDateOfLastYear()).when(mock).getTimestamp();
-        doReturn(UUID.randomUUID()).when(mock).getUuid();
+        when(mock.getAmount()).thenReturn(getRandomBigDecimal());
+        when(mock.getComment()).thenReturn(getRandomString());
+        when(mock.getTimestamp()).thenReturn(getRandomDateOfLastYear());
+        when(mock.getUuid()).thenReturn(UUID.randomUUID());
+        return mock;
+    }
+
+    public static Cost cost(String comment, int amount) {
+        Cost mock = mock(Cost.class);
+        when(mock.getComment()).thenReturn(comment);
+        when(mock.getAmount()).thenReturn(new BigDecimal(amount));
         return mock;
     }
 
     public static Cost cost(DateTime dateTime) {
         Cost cost = mock(Cost.class);
-        doReturn(dateTime.toDate()).when(cost).getTimestamp();
+        when(cost.getTimestamp()).thenReturn(dateTime.toDate());
         return cost;
     }
 
@@ -47,8 +55,8 @@ public class CounteeTestUtils {
     }
 
     public static Cost costWithComment(String comment) {
-        Cost mock = mock(Cost.class);
-        doReturn(comment).when(mock).getComment();
+        Cost mock = getRandomCost();
+        when(mock.getComment()).thenReturn(comment);
         return mock;
     }
     
