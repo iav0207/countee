428e9f7c585e06a6ec4addeb9596b6d4957d6bc3
Alexander Isaev <IsaevAV27@gmail.com>
2018-03-20 10:54:30 +0300

    Cleanup

--- crypt/src/main/java/ru/iav/takoe/countee/crypt/impl/SimpleGostDecryptor.java
+++ crypt/src/main/java/ru/iav/takoe/countee/crypt/impl/SimpleGostDecryptor.java
@@ -1,7 +1,6 @@
 package ru.iav.takoe.countee.crypt.impl;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import javax.xml.bind.DatatypeConverter;
 
 import org.apache.commons.codec.binary.Base64;
 import ru.iav.takoe.countee.crypt.utils.BytesToStringParser;
@@ -22,11 +21,7 @@ class SimpleGostDecryptor extends SimpleGostAlgorithmExecutor implements Decrypt
     @Override
     public String decrypt(String cypher) {
         final byte[] cypherBytes;
-        if (true) {
-            cypherBytes = Base64.decodeBase64(cypher.getBytes());       // Android compatible
-        } else {
-            cypherBytes = DatatypeConverter.parseBase64Binary(cypher);
-        }
+        cypherBytes = Base64.decodeBase64(cypher.getBytes());       // Android compatible
         byte[] endTextBytes = SimpleGostAlgorithm.newDecryptor(cypherBytes, key.getBytes()).execute();
         return parser.parseBytesAsText(endTextBytes).trim();
     }
